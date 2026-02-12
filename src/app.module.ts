d687c91627448981ec65d541743d19f90c39ca91
Alex <welmozhko88@gmail.com>
2026-02-12 23:56:43 +0200

    set up typeorm and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // file based database
+      database: 'db.sqlite', // path to the database file
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
