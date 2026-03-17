80a5dae04775560541ea1dfff3ecaa2f5baa224c
Alex <welmozhko88@gmail.com>
2026-03-17 22:49:56 +0200

    set up user-reports relation

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReportDto } from './reports.dtos';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
 
 @Injectable()
 export class ReportsService {
-  createReport(reportDto: CreateReportDto) {
-    console.log('value ===>', reportDto);
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
   }
 }
